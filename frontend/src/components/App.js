ac4f679da5a055957713a24e8d08f0f6defbe161
Isaac <isaac.andrew.williams@gmail.com>
2020-01-10 15:56:56 +0100

    removed unnecessary import pointing to old folder

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-
 import FileForm from "./Form";
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Basic from "./my-dropzone";
 
 
 const App = () => (
